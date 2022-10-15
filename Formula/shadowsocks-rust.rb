fc521f1917046e595f00c9d5b363f8cf3415e42c
XiaoGuang Zeng <xgzeng@gmail.com>
2022-10-15 19:41:00 +0800

    cleanup

--- Formula/shadowsocks-rust.rb
+++ Formula/shadowsocks-rust.rb
@@ -6,15 +6,6 @@ class ShadowsocksRust < Formula
   license "MIT"
   head "https://github.com/shadowsocks/shadowsocks-rust.git", branch: "master"
 
-#   bottle do
-#     sha256 cellar: :any_skip_relocation, arm64_monterey: "88d62b52c7b054b7c3f8e62a394b3c79b1918b445523677003a7510f32c93a1d"
-#     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cb5ae09531a8143b8c2bf73611c093dccf9314389024d3771aaa0e78f6beb08a"
-#     sha256 cellar: :any_skip_relocation, monterey:       "042cae8f2613b729d11c48991ab4abf97d88995af2c0a63157ec474e48219b36"
-#     sha256 cellar: :any_skip_relocation, big_sur:        "2c14fa5da61271a48cfc8618eba8adaf631ae469ba1594e0d5dbca182a97fc34"
-#     sha256 cellar: :any_skip_relocation, catalina:       "8eeb477f69deb528976bc2234ead8dbf7cc8428fb38fc802a558ceabab27f209"
-#     sha256 cellar: :any_skip_relocation, x86_64_linux:   "70fb502b9f580ff862b61447cca3be865dc3345b9b204581590eae7a6210ec83"
-#   end
-
   depends_on "rust" => :build
 
   def install
@@ -24,9 +15,6 @@ class ShadowsocksRust < Formula
   def post_install
     conf = etc/"shadowsocks-rust/local.json"
     return if conf.exist?
-    # return unless conf.read.include?('username: "@@HOMEBREW-UNBOUND-USER@@"')
-    # inreplace conf, 'username: "@@HOMEBREW-UNBOUND-USER@@"',
-    #                 "username: \"#{ENV["USER"]}\""
     conf.write <<~EOS
       {
         "server":"127.0.0.1",
